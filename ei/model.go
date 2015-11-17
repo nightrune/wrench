89d41ba2d08adfe288c3ef92fc0ba88ae39a238d
Sean Sill <theblacklabcoat@gmail.com>
2015-11-17 15:41:43 -0800

    Library support for geting, listing, and updating code

--- ei/model.go
+++ ei/model.go
@@ -6,9 +6,14 @@ import (
 	"github.com/nightrune/wrench/logging"
   "encoding/base64"
   "encoding/json"
+  "errors"
+  "net/http/httputil"
+  "io/ioutil"
 )
 
 const EI_URL = "https://build.electricimp.com/v4/"
+const MODELS_ENDPOINT = "models"
+const MODELS_REVISIONS_ENDPOINT = "revisions"
 
 type Device struct {
   Id string `json:"id"`
@@ -28,6 +33,36 @@ type Model struct {
 
 type ModelList struct {
   Models []Model `json:"models"`
+}
+
+type BuildError struct {
+	Code string `json:"code"`
+	ShortMessage string `json:"message_short"`
+}
+
+type CodeRevisionResponse struct {
+  Success bool `json:"success"`
+  Revisions CodeRevisionLong `json:"revision"`
+  Error BuildError `json:"error"`
+}
+
+type CodeRevisionsResponse struct {
+  Success bool `json:"success"`
+  Revisions []CodeRevisionShort `json:"revisions"`
+}
+
+type CodeRevisionShort struct {
+  Version int `json:"version"`
+  CreatedAt string `json:"created_at"`
+  ReleaseNotes string `json:"release_notes"`
+}
+
+type CodeRevisionLong struct {
+  Version int `json:"version,omitempty"`
+  CreatedAt string `json:"created_at,omitempty"`
+  DeviceCode string `json:"device_code,omitempty"`
+  AgentCode string `json:"agent_code,omitempty"`
+  ReleaseNotes string `json:"release_notes,omitempty"`
 }
 
 type BuildClient struct {
@@ -54,31 +89,132 @@ func (m BuildClient) SetAuthHeader(request *http.Request) {
   request.Header.Set("Authorization", "Basic " + m.creds)
 }
 
-func (m *BuildClient) ListModels() []Model {
-  data := make([]byte, 100)
-  full_response := new(bytes.Buffer)
-  url := Concat(EI_URL, "models")
-  req, _ := http.NewRequest("GET", url, nil)
+func (m *BuildClient) _complete_request(method string,
+	url string, data []byte)  ([]byte, error) {
+  var req *http.Request
+  if data != nil {
+  	req, _ = http.NewRequest(method, url, bytes.NewBuffer(data))
+  }  else {
+  	req, _ = http.NewRequest(method, url, nil)
+  }
+
   m.SetAuthHeader(req)
+  req.Header.Set("Content-Type", "application/json")
   resp, err := m.http_client.Do(req)
   if err == nil {
-    var n int
-  	for err = nil; err == nil; n, err = resp.Body.Read(data) {
-      full_response.Write(data[:n])
+    dump, err := httputil.DumpResponse(resp, true)
+    logging.Debug(string(dump))
+    full_response, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+    	return full_response, err
     }
-    list := new(ModelList)
-    if err := json.Unmarshal(full_response.Bytes(), list); err != nil {
-      logging.Warn("Failed to unmarshal data from models.. %s", err.Error());
-    }
-    for _, m := range list.Models {
-      logging.Debug("Name: %s", m.Name) 
-    }
-    
-    return nil
+    return full_response, nil
   } else {
+  	return nil, err
+  }	
+}
+
+func (m *BuildClient) ListModels() (*ModelList, error) {
+  list := new(ModelList)
+  full_resp, err := m._complete_request("GET", Concat(EI_URL, "models"), nil)
+  if err != nil {
   	logging.Debug("An error happened during model get, %s", err.Error())
-  	return nil
+  	return list, err
   }
+  
+  if err := json.Unmarshal(full_resp, list); err != nil {
+    logging.Warn("Failed to unmarshal data from models.. %s", err.Error());
+    return list, err
+  }
+
+  return list, nil
+}
+
+func (m *BuildClient) GetCodeRevisionList(model_id string) (
+	[]CodeRevisionShort, error) {
+  var url bytes.Buffer
+  resp := new(CodeRevisionsResponse)
+  url.WriteString(EI_URL)
+  url.WriteString(MODELS_ENDPOINT)
+  url.WriteString("/")
+  url.WriteString(model_id)
+  url.WriteString("/")
+  url.WriteString(MODELS_REVISIONS_ENDPOINT)
+  full_resp, err := m._complete_request("GET", url.String(), nil)
+  if err != nil {
+  	logging.Debug("Failed to get code revisions: %s", err.Error())
+  	return resp.Revisions, err
+  }
+
+
+  if err := json.Unmarshal(full_resp, resp); err != nil {
+    logging.Warn("Failed to unmarshal data from code revision.. %s", err.Error());
+    return resp.Revisions, err
+  }
+  
+  if resp.Success == false {
+  	return resp.Revisions, errors.New("Error When retriveing Code Revisions")
+  }
+  return resp.Revisions, nil
+}
+
+
+func (m *BuildClient) GetCodeRevision(model_id string, build_num string) (CodeRevisionLong, error) {
+  var url bytes.Buffer
+  resp := new(CodeRevisionResponse)
+  url.WriteString(EI_URL)
+  url.WriteString(MODELS_ENDPOINT)
+  url.WriteString("/")
+  url.WriteString(model_id)
+  url.WriteString("/")
+  url.WriteString(MODELS_REVISIONS_ENDPOINT)
+  url.WriteString("/")
+  url.WriteString(build_num)
+  full_resp, err := m._complete_request("GET", url.String(), nil)
+  if err != nil {
+  	logging.Debug("Failed to get code revisions: %s", err.Error())
+  	return resp.Revisions, err
+  }
+
+  if err := json.Unmarshal(full_resp, resp); err != nil {
+    logging.Warn("Failed to unmarshal data from code revision.. %s", err.Error());
+    return resp.Revisions, err
+  }
+  
+  if resp.Success == false {
+  	return resp.Revisions, errors.New("Error When retriveing Code Revisions")
+  }
+  return resp.Revisions, nil
+}
+
+func (m *BuildClient) UpdateCodeRevision(model_id string,
+	request *CodeRevisionLong) (CodeRevisionLong, error) {
+  var url bytes.Buffer
+  resp := new(CodeRevisionResponse)
+  url.WriteString(EI_URL)
+  url.WriteString(MODELS_ENDPOINT)
+  url.WriteString("/")
+  url.WriteString(model_id)
+  url.WriteString("/")
+  url.WriteString(MODELS_REVISIONS_ENDPOINT)
+
+  req_string, err := json.Marshal(request)
+  logging.Debug("Request String for upload: %s", req_string)
+  full_resp, err := m._complete_request("POST", url.String(), req_string)
+  if err != nil {
+  	logging.Debug("Failed to update code revisions: %s", err.Error())
+  	return resp.Revisions, err
+  }
+
+  if err := json.Unmarshal(full_resp, resp); err != nil {
+    logging.Warn("Failed to unmarshal data from code revision update.. %s", err.Error());
+    return resp.Revisions, err
+  }
+  
+  if resp.Success == false {
+  	return resp.Revisions, errors.New("Error When retriveing Code Revisions")
+  }
+  return resp.Revisions, nil
 }
 
 /*
--- ei/model_test.go
+++ ei/model_test.go
@@ -1,12 +1,83 @@
 package ei
 
-import "testing"
 import "github.com/nightrune/wrench/logging"
 
-func TestRunDevice(t *testing.T) {
+const api_key = "1d2da2b7e4e35667283af41ba2458527"
+const model_key = "VqNg23hFUNjg"
+
+func ExampleGetModel() {
   logging.SetLoggingLevel(logging.LOG_DEBUG);
   logging.Info("Starting Test")
-  client := NewBuildClient("1d2da2b7e4e35667283af41ba2458527")
-  client.ListModels();
+  client := NewBuildClient(api_key)
+  model_list, err := client.ListModels();
+  if err != nil {
+  	logging.Fatal("Test Failed %s", err.Error())
+  	return
+  }
+
+  for _, model := range model_list.Models {
+  	logging.Info("Id: %s, Name: %s", model.Id, model.Name)
+  }
   logging.Info("Ending Test")
+  //Output:
+}
+
+func ExampleGetCodeRevisions() {
+  logging.SetLoggingLevel(logging.LOG_DEBUG);
+  logging.Info("Starting Test")
+  client := NewBuildClient(api_key)
+  revisions, err := client.GetCodeRevisionList(model_key);
+  if err != nil {
+  	logging.Fatal("Test Failed %s", err.Error())
+  	return
+  }
+
+  for _, revision := range revisions {
+  	logging.Info(string(revision.CreatedAt))
+	logging.Info(`Version: %d
+  	              CreatedAt: %s,
+	  	          ReleaseNotes: %s,`, 
+	  	          revision.Version,
+	  	          revision.CreatedAt,
+	  	          revision.ReleaseNotes)
+  }
+  logging.Info("Ending Test")
+  //Output:
+}
+
+func ExampleGetCodeRevision() {
+  logging.SetLoggingLevel(logging.LOG_DEBUG);
+  logging.Info("Starting Test")
+  client := NewBuildClient(api_key)
+  revision, err := client.GetCodeRevision(model_key, "1");
+  if err != nil {
+  	logging.Fatal("Test Failed %s", err.Error())
+  	return
+  }
+
+  logging.Info(string(revision.CreatedAt))
+  logging.Info(`Version: %d
+  	            CreatedAt: %s,
+  	            ReleaseNotes: %s,
+  	            AgentCode: %s,
+  	            DeviceCode: %s,`,
+  	            revision.Version,
+  	            revision.CreatedAt,
+  	            revision.ReleaseNotes,
+  	            revision.AgentCode,
+  	            revision.DeviceCode)
+  logging.Info("Ending Test")
+  //Output:
+}
+
+func ExampleUploadCode() {
+	logging.SetLoggingLevel(logging.LOG_DEBUG);
+  	logging.Info("Starting Test")
+	request := new(CodeRevisionLong)
+	client := NewBuildClient(api_key)
+	request.AgentCode = `server.log("More Agent Code!")`
+	request.DeviceCode = `server.log("More Device Code!")`
+  	client.UpdateCodeRevision(model_key, request);
+	logging.Info("Ending Test")
+	//Output:
 }
